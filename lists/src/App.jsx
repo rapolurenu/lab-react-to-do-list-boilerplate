7586060a5aa6398cf776d154de8de1151322e5b7
rapolurenu <pji.rapolu.renu@kalvium.community>
2024-05-04 11:41:20 +0530

    done

---
+++ lists/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// eslint-disable-next-line no-unused-vars
+import dolists from './components/dolists'
+
+
+
+function App() {
+    return (
+        <div>
+            <dolists />
+        </div>
+    )
+}
+
+export default App
